f9fa3de915606a4ff6d18ed2d906f971c3fc5b0f
kx1t <ramonkolb@gmail.com>
2022-02-01 11:17:26 -0500

    updates

--- buildnow.sh
+++ buildnow.sh
@@ -11,6 +11,7 @@ git checkout $BRANCH || exit 2
 
 git pull
 
-DOCKER_BUILDKIT=1 docker buildx build --progress=plain --compress --push $2 --platform linux/armhf,linux/arm64,linux/amd64,linux/i386 --tag kx1t/screenshot:$TAG .
+#DOCKER_BUILDKIT=1 docker buildx build --progress=plain --compress --push $2 --platform linux/armhf,linux/arm64,linux/amd64,linux/i386 --tag kx1t/screenshot:$TAG .
+DOCKER_BUILDKIT=1 docker buildx build --compress --push $2 --platform linux/armhf,linux/arm64,linux/amd64,linux/i386 --tag kx1t/screenshot:$TAG .
 
 popd
