02cb403f761df103d5f175eefdfb5b9ff21ed71b
kx1t <ramonkolb@gmail.com>
2022-02-01 11:16:38 -0500

    updates

--- buildnow.sh
+++ buildnow.sh
@@ -11,6 +11,6 @@ git checkout $BRANCH || exit 2
 
 git pull
 
-DOCKER_BUILDKIT=1 docker buildx build --progress=plain --compress --push $2 --platform linux/armhf,linux/arm64 --tag kx1t/webproxy:$TAG .
+DOCKER_BUILDKIT=1 docker buildx build --progress=plain --compress --push $2 --platform linux/armhf,linux/arm64,linux/amd64,linux/i386 --tag kx1t/screenshot:$TAG .
 
 popd
