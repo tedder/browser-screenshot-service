33b36e4b867b698952b60f90eb86ff46590ef4f0
kx1t <ramonkolb@gmail.com>
2022-12-31 09:50:11 -0500

    updated build script

--- buildnow.sh
+++ buildnow.sh
@@ -4,14 +4,11 @@ set -x
 
 [[ "$1" != "" ]] && BRANCH="$1" || BRANCH=main
 [[ "$BRANCH" == "main" ]] && TAG="latest" || TAG="$BRANCH"
-
+[[ -z "$ARCHS" ]] && ARCHS="linux/armhf,linux/arm64,linux/amd64"
 # rebuild the container
-pushd ~/git/browser-screenshot-service
 git checkout $BRANCH || exit 2
 
 git pull
 
 #DOCKER_BUILDKIT=1 docker buildx build --progress=plain --compress --push $2 --platform linux/armhf,linux/arm64,linux/amd64,linux/i386 --tag kx1t/screenshot:$TAG .
-DOCKER_BUILDKIT=1 docker buildx build --compress --push $2 --platform linux/armhf,linux/arm64,linux/amd64,linux/i386 --tag kx1t/screenshot:$TAG .
-
-popd
+DOCKER_BUILDKIT=1 docker buildx build --compress --push $2 --platform $ARCHS --tag kx1t/screenshot:$TAG .
