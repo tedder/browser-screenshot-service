1a9ccf6c32572ee3ceabaeaa7e65ce1914d63b29
kx1t <kx1t@amsat.org>
2023-02-06 08:03:44 -0500

    add User Agent to ensure plane pix are pulled correctly

--- snapapi.py
+++ snapapi.py
@@ -84,6 +84,7 @@ def get_screenshot(icao):
   co.add_argument("--disable-dev-shm-usage")
   co.add_argument("--disable-browser-side-navigation")
   co.add_argument("--disable-gpu")
+  co.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
   #co.add_argument("--incognito")
 
   # thrash on the filesystem, better than the page crashing
